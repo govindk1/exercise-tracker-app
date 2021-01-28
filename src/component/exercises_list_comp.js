931d50814d3992a63d5120479e6999aa4ee22c48
govind <kotharigovind1122@gmail.com>
2021-01-28 12:52:24 +0530

    added all exercise list

--- src/component/exercises_list_comp.js
+++ src/component/exercises_list_comp.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+var moment = require('moment');
 
 const ExercisesList = () => {
+
+    const [k1, setk1] = useState({hits:[]});
+
+    useEffect(() => {
+
+        async function all_exercises(){
+            const response = await axios.get('http://localhost:5000/exercises/')
+            
+            
+            setk1({hits:[...response.data]})
+            
+        }
+            
+       all_exercises()
+        
+    }, [])
+
+    
     return (
         <div>
-           <p>You are on the Excercises List component!</p> 
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+
+          {!k1.hits.length}{
+    
+            k1.hits.map((exercise) =>  {
+
+                
+
+                return(
+                    <tr key={k1.id}>
+                    <td key={k1.id}>{exercise.username}</td>
+                    <td key={k1.id}>{exercise.description}</td>
+                    <td key={k1.id}>{exercise.duration}</td>
+                    <td key={k1.id}>{moment(exercise.date).utc().format('MM/DD/YYYY')}</td>
+                    
+                </tr>
+                )}
+            )
+          }
+            
+          </tbody>
+        </table>
         </div>
     )
 }
